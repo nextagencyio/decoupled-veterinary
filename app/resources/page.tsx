8a0a3c15b2bd2df57f1a15f4f48ee9f0227da0ac
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:47 -0500

    fix: mark header-dependent routes as dynamic

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import PetResourceCard from '../components/PetResourceCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Pet Resources | Veterinary',
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import ServiceCard from '../components/ServiceCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Services | Veterinary',
